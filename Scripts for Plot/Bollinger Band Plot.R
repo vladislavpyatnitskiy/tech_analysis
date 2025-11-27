35c52e657aad060598c3ec73ba5678f575a092ea
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-11-27 11:06:23 +0500

    Enhance bollinger.band function with date filters
    Refactor bollinger.band function to include optional start and end dates for data retrieval. Update plotting logic to enhance clarity and maintainability.

--- Scripts for Plot/Bollinger Band Plot.R
+++ Scripts for Plot/Bollinger Band Plot.R
@@ -1,6 +1,23 @@
-library("timeSeries")
+lapply(c("quantmod", "timeSeries"), require, character.only = T) # Libs
 
-bollinger.band <- function(x, ts=20){
+bollinger.band <- function(x, s=NULL, e=NULL, ts=20, data=T){
+  
+  p <- NULL # 4 scenarios: no dates, only start or end dates, both dates
+  src <- "yahoo"
+  
+  getData <- function(A, s, e) {
+    if (is.null(s) && is.null(e)) return(getSymbols(A, src=src, auto.assign=F)) 
+    if (is.null(e)) return(getSymbols(A, from = s, src=src, auto.assign=F)) 
+    if (is.null(s)) return(getSymbols(A, to = e, src=src, auto.assign=F)) 
+    return(getSymbols(A, from = s, to = e, src=src, auto.assign=F)) 
+  }
+  if (data){ for (A in x){ p <- cbind(p, getData(A, s, e)[,4]) } # Join data
+    
+    p <- p[apply(p, 1, function(x) all(!is.na(x))),] # Get rid of NA
+    
+    colnames(p) <- x # Put the tickers in column names
+    
+    x <- as.timeSeries(p) } # Make it time series and display
   
   ts_n <- ts - 1
   
@@ -41,7 +58,9 @@ bollinger.band <- function(x, ts=20){
     
     abline(h = 0) # Add black horizontal line at break even point
     
-    for (m in 2:(ncol(p))){ lines(p[,m], col = m, lwd = 2) } # Plot indices
+    col = c(1, 4, 3, 2)
+    
+    for (m in 2:(ncol(p))){ lines(p[,m], col = col[m], lwd = 2) } # other lines
     
     par(mar = c(8, rep(4, 3))) # Define borders of the plot
     
@@ -49,7 +68,7 @@ bollinger.band <- function(x, ts=20){
       x = "bottom",
       inset = c(0, -0.2),
       legend = colnames(p),
-      col = seq(ncol(p)),
+      col = col,
       lwd = 2,
       cex = .85,
       bty = "n",
@@ -60,4 +79,4 @@ bollinger.band <- function(x, ts=20){
     on.exit(par(par(no.readonly = T))) # Show legend with names
   }
 }
-bollinger.band(stock_data)
+bollinger.band(c("GOOGL", "ZIM"), s="2022-01-01")
