e4ae5c22b593826308d916a2aa370f1f58a043ec
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-01-08 14:13:40 +0500

    Update Moving Average Plot for Russian Stocks.R
    Added download message

--- Scripts for Plot/Moving Average Plot for Russian Stocks.R
+++ Scripts for Plot/Moving Average Plot for Russian Stocks.R
@@ -12,7 +12,14 @@ lines.plt.ma.rus <- function(x, s=NULL, e=NULL, ts=50, data=T, split=T, all=F){
     return(get_candles(A, from = s, till = e, interval = 'daily')) 
   }
   for (A in x){ D <- as.data.frame(getData(A, s, e)[,c(3,8)])
-    
+
+    message(
+      sprintf(
+        "%s is downloaded (%s / %s)", 
+        A, which(x == A), length(x)
+      )
+    ) # Download message
+               
     D <- D[!duplicated(D),] # Remove duplicates
     
     D <- xts(D[,1], order.by = as.Date(D[,2])) # Move dates to row names
