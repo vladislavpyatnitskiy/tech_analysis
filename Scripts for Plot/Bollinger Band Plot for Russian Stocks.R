f2745a5e0165895a287bea5a3f41f4be8cf74702
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-01-10 11:43:02 +0500

    Update Bollinger Band Plot for Russian Stocks.R
    Added download message

--- Scripts for Plot/Bollinger Band Plot for Russian Stocks.R
+++ Scripts for Plot/Bollinger Band Plot for Russian Stocks.R
@@ -12,7 +12,14 @@ bollinger.band.rus <- function(x, s=NULL, e=NULL, ts=20, data=T){
     return(get_candles(A, from = s, till = e, interval = 'daily')) 
   }
   for (A in x){ D <- as.data.frame(getData(A, s, e)[,c(3,8)])
-    
+
+    message(
+      sprintf(
+        "%s is downloaded (%s / %s)", 
+        A, which(x == A), length(x)
+      )
+    ) # Download message
+               
     D <- D[!duplicated(D),] # Remove duplicates
     
     D <- xts(D[,1], order.by = as.Date(D[,2])) # Move dates to row names
